02548e857070ddd7811609e0334c3f769ff0ccbc
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-11 21:31:43 -0600

    implemented LRUKReplacer::size()

--- src/buffer/lru_k_replacer.rs
+++ src/buffer/lru_k_replacer.rs
@@ -224,4 +224,11 @@ impl LRUKReplacer {
             return;
         }
     }
+
+    pub fn size(&self) -> usize {
+        // lock
+        let size = self.cur_size;
+        // unlock
+        return size;
+    }
 }
