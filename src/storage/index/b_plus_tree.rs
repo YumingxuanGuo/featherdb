1c1c6203bef84cbe5bdc54764a35c4eb6de82f1e
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-15 17:23:02 -0600

    added helper functions for typecasting

--- src/storage/index/b_plus_tree.rs
+++ src/storage/index/b_plus_tree.rs
@@ -46,7 +46,7 @@ impl BPlusTree {
 }
 
 impl Store for BPlusTree {
-    fn insert(&mut self, key: &String, value: &Vec<u8>) -> crate::error::Result<()> {
+    fn insert(&mut self, key: KeyType, value: ValueType) -> crate::error::Result<()> {
         Ok(())
     }
 
@@ -54,57 +54,71 @@ impl Store for BPlusTree {
         Ok(())
     }
 
-    fn get_value(&mut self, key: KeyType) -> Option<ValueType> {
+    fn get_value(&mut self, key: &KeyType) -> Option<ValueType> {
         unsafe {
-            let page = self.buffer_pool_manager.fetch_page(self.root_page_id)
-                .expect("fetch failed");
-            let data_ptr: *const [u8; PAGE_SIZE] = &page.data;
-            let tree_page_ptr: *const BPlusTreeInternalPage = data_ptr.cast::<BPlusTreeInternalPage>();
-            let mut tree_page = tree_page_ptr.as_ref().unwrap();
-            while !tree_page.is_leaf_page() {
-                // brute force search; use binary search later
-                if key < tree_page.array[1].0 {
-                    let page = self.buffer_pool_manager
-                        .fetch_page(tree_page.array[0].1)
-                        .expect("fetch failed");
-                    let data_ptr: *const [u8; PAGE_SIZE] = &page.data;
-                    let tree_page_ptr: *const BPlusTreeInternalPage = data_ptr.cast::<BPlusTreeInternalPage>();
-                    tree_page = tree_page_ptr.as_ref().unwrap();
-                } else {
-                    for i in 1..tree_page.get_size() {
-                        let left_key = tree_page.array[i as usize].0;
-                        let right_key = tree_page.array[(i+1) as usize].0;
-                        if left_key <= key && key < right_key {
-                            let page = self.buffer_pool_manager
-                                .fetch_page(tree_page.array[i as usize].1)
-                                .expect("fetch failed");
-                            let data_ptr: *const [u8; PAGE_SIZE] = &page.data;
-                            let tree_page_ptr: *const BPlusTreeInternalPage = data_ptr.cast::<BPlusTreeInternalPage>();
-                            tree_page = tree_page_ptr.as_ref().unwrap();
-                            break;
-                        }
-                    }
-                    let last_key_index: usize = tree_page.get_size() as usize;
-                    if key >= tree_page.array[last_key_index].0 {
-                        let page = self.buffer_pool_manager
-                            .fetch_page(tree_page.array[last_key_index].1)
-                            .expect("fetch failed");
-                        let data_ptr: *const [u8; PAGE_SIZE] = &page.data;
-                        let tree_page_ptr: *const BPlusTreeInternalPage = data_ptr.cast::<BPlusTreeInternalPage>();
-                        tree_page = tree_page_ptr.as_ref().unwrap();
-                    }
-                }
-            }
-            assert!(tree_page.is_leaf_page());
-            let tree_page_ptr: *const BPlusTreeInternalPage = tree_page;
-            let leaf_page_ptr: *const BPlusTreeLeafPage = tree_page_ptr.cast::<BPlusTreeLeafPage>();
-            let tree_page = leaf_page_ptr.as_ref().unwrap();
-            for (k, v) in tree_page.array {
-                if key == k {
+            let leaf_page_id: PageID = self.find_leaf_page_id(key);
+            let leaf_page_ptr =
+                self.fetch_page_as_const_ptr::<BPlusTreeLeafPage>(leaf_page_id);
+            let leaf_page = leaf_page_ptr.as_ref()
+                .expect("raw-pointer to reference failed");
+            for (k, v) in leaf_page.array {
+                if *key == k {
                     return Some(v);
                 }
             }
         }
         return None;
+    }
+}
+
+impl BPlusTree {
+    unsafe fn find_leaf_page_id(&mut self, key: &KeyType) -> PageID {
+        let tree_page_ptr =
+            self.fetch_page_as_const_ptr::<BPlusTreeInternalPage>(self.root_page_id);
+        let mut tree_page = tree_page_ptr.as_ref()
+            .expect("raw-pointer to reference failed");
+        while !tree_page.is_leaf_page() {
+            // brute force search; use binary search later
+            if *key < tree_page.array[1].0 {
+                let tree_page_ptr =
+                    self.fetch_page_as_const_ptr::<BPlusTreeInternalPage>(tree_page.array[0].1);
+                tree_page = tree_page_ptr.as_ref().expect("raw-pointer to reference failed");
+            } else {
+                for i in 1..tree_page.get_size() {
+                    let left_key = tree_page.array[i as usize].0;
+                    let right_key = tree_page.array[(i+1) as usize].0;
+                    if left_key <= *key && *key < right_key {
+                        let tree_page_ptr =
+                            self.fetch_page_as_const_ptr::<BPlusTreeInternalPage>(tree_page.array[i as usize].1);
+                        tree_page = tree_page_ptr.as_ref().expect("raw-pointer to reference failed");
+                        break;
+                    }
+                }
+                let last_key_index: usize = tree_page.get_size() as usize;
+                if *key >= tree_page.array[last_key_index].0 {
+                    let tree_page_ptr =
+                        self.fetch_page_as_const_ptr::<BPlusTreeInternalPage>(tree_page.array[last_key_index].1);
+                    tree_page = tree_page_ptr.as_ref().expect("raw-pointer to reference failed");
+                }
+            }
+        }
+        assert!(tree_page.is_leaf_page());
+        return tree_page.get_page_id();
+    }
+    
+    unsafe fn fetch_page_as_const_ptr<Dst>(&mut self, page_id: PageID) -> *const Dst {
+        let page = self.buffer_pool_manager.fetch_page(page_id)
+            .expect("fetch failed");
+        let data_ptr: *const [u8; PAGE_SIZE] = &page.data;
+        let tree_page_ptr: *const Dst = data_ptr.cast::<Dst>();
+        return tree_page_ptr;
+    }
+
+    unsafe fn fetch_page_as_mut_ptr<Dst>(&mut self, page_id: PageID) -> *mut Dst {
+        let page = self.buffer_pool_manager.fetch_page(page_id)
+            .expect("fetch failed");
+        let data_ptr: *mut [u8; PAGE_SIZE] = &mut page.data;
+        let tree_page_ptr: *mut Dst = data_ptr.cast::<Dst>();
+        return tree_page_ptr;
     }
 }
