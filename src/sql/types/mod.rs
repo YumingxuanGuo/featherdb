893b79dfeb68529f919524ada3bdc143144ebdc3
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-23 22:53:52 -0600

    added Display trait for Value

--- src/sql/types/mod.rs
+++ src/sql/types/mod.rs
@@ -21,6 +21,24 @@ pub enum Value {
     String(String),
 }
 
+impl std::cmp::Eq for Value {}
+
+impl std::fmt::Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.write_str(
+            match self {
+                Self::Null => "NULL".to_string(),
+                Self::Boolean(b) if *b => "TRUE".to_string(),
+                Self::Boolean(_) => "FALSE".to_string(),
+                Self::Integer(i) => i.to_string(),
+                Self::Float(f) => f.to_string(),
+                Self::String(s) => s.clone(),
+            }
+            .as_ref(),
+        )
+    }
+}
+
 /// A row of values
 pub type Row = Vec<Value>;
 
