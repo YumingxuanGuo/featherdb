92a16208bd4af7da17faaed4792cb23488987609
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-23 18:13:22 -0600

    implemented Table

--- src/sql/schema.rs
+++ src/sql/schema.rs
@@ -2,7 +2,7 @@
 use crate::error::{Error, Result};
 use serde_derive::{Deserialize, Serialize};
 
-use super::types::{DataType, Value};
+use super::{types::{DataType, Value}, engine::SqlTxn};
 
 /// The catalog stores schema information
 pub trait Catalog {
@@ -34,6 +34,48 @@ pub struct Table {
     pub columns: Vec<Column>,
 }
 
+impl Table {
+    /// Creates a new table schema
+    pub fn new(name: String, columns: Vec<Column>) -> Result<Self> {
+        Ok(Self { name, columns })
+    }
+
+    /// Returns the primary key value of a row
+    pub fn get_row_primary_key(&self, row: &[Value]) -> Result<Value> {
+        Ok(Value::Null)
+    }
+
+    /// Validates the table schema.
+    pub fn validate_schema(&self, txn: &mut dyn SqlTxn) -> Result<()> {
+        if self.columns.is_empty() {
+            return Err(Error::Value(format!("Table {} has no columns", self.name)));
+        }
+        // Checks if there is exactly one primary key.
+        match self.columns.iter().filter(|c| c.is_primary).count() {
+            1 => {},
+            0 => return Err(Error::Value(format!("No primary key in table {}", self.name))),
+            _ => return Err(Error::Value(format!("Multiple primary keys in table {}", self.name))),
+        };
+        // Checks if each column is valid.
+        for column in &self.columns {
+            column.validate_schema(self, txn)?;
+        }
+        Ok(())
+    }
+
+    /// Validates a row
+    pub fn validate_row(&self, row: &[Value], txn: &mut dyn SqlTxn) -> Result<()> {
+        if row.len() != self.columns.len() {
+            return Err(Error::Value(format!("Invalid row size for table {}", self.name)));
+        }
+        let primary_key = self.get_row_primary_key(row)?;
+        for (column, value) in self.columns.iter().zip(row.iter()) {
+            column.validate_value(self, &primary_key, value, txn)?;
+        }
+        Ok(())
+    }
+}
+
 pub type Tables = Box<dyn DoubleEndedIterator<Item = Table> + Send>;
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
@@ -54,4 +96,22 @@ pub struct Column {
     pub references: Option<String>,
     /// Whether this column is indexed
     pub is_indexed: bool,
+}
+
+impl Column {
+    /// Validates the column schema
+    pub fn validate_schema(&self, table: &Table, txn: &mut dyn SqlTxn) -> Result<()> {
+        Ok(())
+    }
+
+    /// Validates a column value
+    pub fn validate_value(
+        &self,
+        table: &Table,
+        primary_key: &Value,
+        value: &Value,
+        txn: &mut dyn SqlTxn,
+    ) -> Result<()> {
+        Ok(())
+    }
 }
