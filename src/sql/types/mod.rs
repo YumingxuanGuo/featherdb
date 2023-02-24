0bd1356a6fa741a667ea492c7cef7b3bb5680f05
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-24 10:29:10 -0600

    added hashing function, Cow transformation, and other helper functions for Value

--- src/sql/types/mod.rs
+++ src/sql/types/mod.rs
@@ -1,4 +1,6 @@
 
+use std::{hash::{Hash, Hasher}, borrow::Cow};
+
 use serde_derive::{Deserialize, Serialize};
 
 use crate::error::Result;
@@ -23,6 +25,32 @@ pub enum Value {
 
 impl std::cmp::Eq for Value {}
 
+#[allow(clippy::derive_hash_xor_eq)]
+impl Hash for Value {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.datatype().hash(state);
+        match self {
+            Value::Null => self.hash(state),
+            Value::Boolean(v) => v.hash(state),
+            Value::Integer(v) => v.hash(state),
+            Value::Float(v) => v.to_be_bytes().hash(state),
+            Value::String(v) => v.hash(state),
+        }
+    }
+}
+
+impl<'a> From<Value> for Cow<'a, Value> {
+    fn from(v: Value) -> Self {
+        Cow::Owned(v)
+    }
+}
+
+impl<'a> From<&'a Value> for Cow<'a, Value> {
+    fn from(v: &'a Value) -> Self {
+        Cow::Borrowed(v)
+    }
+}
+
 impl std::fmt::Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         f.write_str(
@@ -36,6 +64,19 @@ impl std::fmt::Display for Value {
             }
             .as_ref(),
         )
+    }
+}
+
+impl Value {
+    /// Returns the value's datatype, or None for null values
+    pub fn datatype(&self) -> Option<DataType> {
+        match self {
+            Self::Null => None,
+            Self::Boolean(_) => Some(DataType::Boolean),
+            Self::Integer(_) => Some(DataType::Integer),
+            Self::Float(_) => Some(DataType::Float),
+            Self::String(_) => Some(DataType::String),
+        }
     }
 }
 
