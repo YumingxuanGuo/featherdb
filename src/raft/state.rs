e866d9bb74a4a3659905df4880dce7fd41939bc4
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-25 17:41:35 -0600

    added raft's state trait

--- src/raft/state.rs
+++ src/raft/state.rs
@@ -1,6 +1,14 @@
-
+use crate::error::Result;
 
 /// A Raft-managed state machine.
 pub trait State: Send {
+    /// Returns the last applied index from the state machine, used when initializing the driver.
+    fn applied_index(&self) -> u64;
 
+    /// Mutates the state machine. If the state machine returns Error::Internal, the Raft node
+    /// halts. For any other error, the state is applied and the error propagated to the caller.
+    fn mutate(&mut self, index: u64, command: Vec<u8>) -> Result<Vec<u8>>;
+
+    /// Queries the state machine. All errors are propagated to the caller.
+    fn query(&self, command: Vec<u8>) -> Result<Vec<u8>>;
 }
