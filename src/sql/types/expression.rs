dec259cab20b3954637942fc1ef872fa5a1fca46
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-25 13:55:05 -0600

    added expression skeletons

--- src/sql/types/expression.rs
+++ src/sql/types/expression.rs
@@ -1,5 +1,16 @@
+use crate::error::Result;
+
+use super::{Row, Value};
+
 
 
 pub enum Expression {
     
+}
+
+impl Expression {
+    /// Evaluates an expression to a value, given an environment
+    pub fn evaluate(&self, row: Option<&Row>) -> Result<Value> {
+        Ok(Value::Null)
+    }
 }
