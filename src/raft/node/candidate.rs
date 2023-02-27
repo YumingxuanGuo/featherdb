8dd767e72768002e3a4630dd599d14d5a7288d13
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-02-27 17:30:59 -0600

    code format change

--- src/raft/node/candidate.rs
+++ src/raft/node/candidate.rs
@@ -1,11 +1,25 @@
+use rand::Rng;
+
+use super::{ELECTION_TIMEOUT_MIN, ELECTION_TIMEOUT_MAX};
+
 
 
 pub struct Candidate {
-
+    /// Ticks elapsed since election start.
+    election_ticks: u64,
+    /// Election timeout, in ticks.
+    election_timeout: u64,
+    /// Votes received (including ourself).
+    vote_count: u64,
 }
 
 impl Candidate {
     pub fn new() -> Self {
-        todo!()
+        Self { 
+            vote_count: 1,
+            election_ticks: 0, 
+            election_timeout: rand::thread_rng()
+                .gen_range(ELECTION_TIMEOUT_MIN..=ELECTION_TIMEOUT_MAX), 
+        }
     }
 }
--- src/raft/node/follower.rs
+++ src/raft/node/follower.rs
@@ -27,11 +27,11 @@ impl Follower {
     /// Creates a new follower role.
     pub fn new(leader: Option<&str>, voted_for: Option<&str>) -> Self {
         Self {
+            voted_for: voted_for.map(String::from),
             leader: leader.map(String::from),
             leader_seen_ticks: 0,
             leader_seen_timeout: rand::thread_rng()
                 .gen_range(ELECTION_TIMEOUT_MIN..=ELECTION_TIMEOUT_MAX),
-            voted_for: voted_for.map(String::from),
         }
     }
 }
