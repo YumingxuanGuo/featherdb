fb5a803eeb2b7f1dd8843db7805eca912d9af878
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-06 08:22:35 -0600

    implemented BlockIterator

--- src/storage/kv/structure/lsm_tree/block.rs
+++ src/storage/kv/structure/lsm_tree/block.rs
@@ -1,8 +1,5 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
-use std::sync::Arc;
-use bytes::{Bytes, BufMut};
+use std::{sync::Arc, usize};
+use bytes::{Bytes, BufMut, Buf};
 
 pub const SIZEOF_U16: usize = std::mem::size_of::<u16>();
 
@@ -95,42 +92,81 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut iter = BlockIterator::new(block);
+        iter.seek_to_first();
+        iter
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: &[u8]) -> Self {
-        unimplemented!()
+        let mut iter = BlockIterator::new(block);
+        iter.seek_to_key(key);
+        iter
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> &[u8] {
-        unimplemented!()
+        debug_assert!(!self.key.is_empty(), "invalid iterator");
+        &self.key
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        debug_assert!(!self.key.is_empty(), "invalid iterator");
+        &self.value
     }
 
     /// Returns true if the iterator is valid.
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
+    }
+
+    fn seek_to_idx(&mut self, idx: usize) {
+        if idx >= self.block.offsets.len() {
+            self.key.clear();
+            self.value.clear();
+            return;
+        }
+        let offset = self.block.offsets[idx] as usize;
+        let mut entry = &self.block.data[offset..];
+        let key_len = entry.get_u16() as usize;
+        self.key = entry[..key_len].into();
+        entry.advance(key_len);
+        let value_len = entry.get_u16() as usize;
+        self.value = entry[..value_len].into();
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.idx = 0;
+        self.seek_to_idx(self.idx);
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.idx += 1;
+        self.seek_to_idx(self.idx);
     }
 
     /// Seek to the first key that >= `key`.
     pub fn seek_to_key(&mut self, key: &[u8]) {
-        unimplemented!()
+        let mut low = 0;
+        let mut high = self.block.offsets.len();
+        while low < high {
+            let mid = (low + high) / 2;
+            self.seek_to_idx(mid);
+            assert!(self.is_valid());
+            match self.key().cmp(key) {
+                std::cmp::Ordering::Less => low = mid + 1,
+                std::cmp::Ordering::Equal => {
+                    self.idx = low;
+                    return;
+                }
+                std::cmp::Ordering::Greater => high = mid,
+            }
+        }
+        self.seek_to_idx(low);
+        self.idx = low;
     }
 }
 
@@ -151,18 +187,22 @@ fn test_block_build_full() {
     builder.build();
 }
 
+#[cfg(test)]
 fn key_of(idx: usize) -> Vec<u8> {
     format!("key_{:03}", idx * 5).into_bytes()
 }
 
+#[cfg(test)]
 fn value_of(idx: usize) -> Vec<u8> {
     format!("value_{:010}", idx).into_bytes()
 }
 
+#[cfg(test)]
 fn num_of_keys() -> usize {
     100
 }
 
+#[cfg(test)]
 fn generate_block() -> Block {
     let mut builder = BlockBuilder::new(10000);
     for idx in 0..num_of_keys() {
@@ -193,6 +233,7 @@ fn test_block_decode() {
     assert_eq!(block.data, decoded_block.data);
 }
 
+#[cfg(test)]
 fn as_bytes(x: &[u8]) -> Bytes {
     Bytes::copy_from_slice(x)
 }
