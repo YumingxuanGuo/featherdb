836c93bf492fe9ab07c9d886eb022800460373a7
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-07 22:14:49 -0600

    implemented LSM-tree::MergeIterator

--- src/storage/kv/structure/lsm_tree/iterators.rs
+++ src/storage/kv/structure/lsm_tree/iterators.rs
@@ -1,6 +1,11 @@
+#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
+#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+
 use std::cmp::{self};
 use std::collections::binary_heap::PeekMut;
 use std::collections::BinaryHeap;
+
+use bytes::Bytes;
 
 use crate::error::Result;
 
@@ -57,19 +62,19 @@ impl<I: StorageIterator> MergeIterator<I> {
         if iters.is_empty() {
             return Self {
                 iters: BinaryHeap::new(),
-                current: None,
+                current: None
             };
         }
 
         let mut heap = BinaryHeap::new();
-
-        if iters.iter().all(|x| !x.is_valid()) {
-            // All invalid, select the last one as the current.
+        
+        // All invalid, select the last one as the current.
+        if iters.iter().all(|iter| !iter.is_valid()) {
             let mut iters = iters;
             return Self {
                 iters: heap,
-                current: Some(HeapWrapper(0, iters.pop().unwrap())),
-            };
+                current: iters.pop().map(|iter|HeapWrapper(0, iter)),
+            }
         }
 
         for (idx, iter) in iters.into_iter().enumerate() {
@@ -78,50 +83,41 @@ impl<I: StorageIterator> MergeIterator<I> {
             }
         }
 
-        let current = heap.pop().unwrap();
+        let current = heap.pop();
         Self {
             iters: heap,
-            current: Some(current),
+            current,
         }
     }
 }
 
 impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
     fn key(&self) -> &[u8] {
-        unsafe { self.current.as_ref().unwrap_unchecked() }.1.key()
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unsafe { self.current.as_ref().unwrap_unchecked() }
-            .1
-            .value()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
         self.current
             .as_ref()
-            .map(|x| x.1.is_valid())
+            .map(|wrapper| wrapper.1.is_valid())
             .unwrap_or(false)
     }
 
     fn next(&mut self) -> Result<()> {
-        let current = unsafe { self.current.as_mut().unwrap_unchecked() };
-        // Pop the item out of the heap if they have the same value.
-        while let Some(mut inner_iter) = self.iters.peek_mut() {
-            debug_assert!(
-                inner_iter.1.key() >= current.1.key(),
-                "heap invariant violated"
-            );
-            if inner_iter.1.key() == current.1.key() {
-                // Case 1: an error occurred when calling `next`.
-                if let e @ Err(_) = inner_iter.1.next() {
-                    PeekMut::pop(inner_iter);
-                    return e;
-                }
+        let current = self.current.as_mut().unwrap();
 
-                // Case 2: iter is no longer valid.
-                if !inner_iter.1.is_valid() {
-                    PeekMut::pop(inner_iter);
+        while let Some(mut iter) = self.iters.peek_mut() {
+            if iter.1.key() == current.1.key() {
+                if let err @ Err(_) = iter.1.next() {
+                    PeekMut::pop(iter);
+                    return err;
+                }
+                if !iter.1.is_valid() {
+                    PeekMut::pop(iter);
                 }
             } else {
                 break;
@@ -130,7 +126,6 @@ impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
 
         current.1.next()?;
 
-        // If the current iterator is invalid, pop it out of the heap and select the next one.
         if !current.1.is_valid() {
             if let Some(iter) = self.iters.pop() {
                 *current = iter;
@@ -138,10 +133,9 @@ impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
             return Ok(());
         }
 
-        // Otherwise, compare with heap top and swap if necessary.
-        if let Some(mut inner_iter) = self.iters.peek_mut() {
-            if *current < *inner_iter {
-                std::mem::swap(&mut *inner_iter, current);
+        if let Some(mut iter) = self.iters.peek_mut() {
+            if *current < *iter {
+                std::mem::swap(&mut *iter, current);
             }
         }
 
@@ -154,74 +148,326 @@ impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
-    choose_a: bool,
+    // Add fields as need
 }
 
 impl<A: StorageIterator, B: StorageIterator> TwoMergeIterator<A, B> {
-    fn choose_a(a: &A, b: &B) -> bool {
-        if !a.is_valid() {
-            return false;
-        }
-        if !b.is_valid() {
-            return true;
-        }
-        a.key() < b.key()
-    }
-
-    fn skip_b(&mut self) -> Result<()> {
-        if self.a.is_valid() {
-            while self.b.is_valid() && self.b.key() == self.a.key() {
-                self.b.next()?;
-            }
-        }
-        Ok(())
-    }
-
     pub fn create(a: A, b: B) -> Result<Self> {
-        let mut iter = Self {
-            choose_a: false,
-            a,
-            b,
-        };
-        iter.skip_b()?;
-        iter.choose_a = Self::choose_a(&iter.a, &iter.b);
-        Ok(iter)
+        unimplemented!()
     }
 }
 
 impl<A: StorageIterator, B: StorageIterator> StorageIterator for TwoMergeIterator<A, B> {
     fn key(&self) -> &[u8] {
-        if self.choose_a {
-            self.a.key()
-        } else {
-            self.b.key()
-        }
+        unimplemented!()
     }
 
     fn value(&self) -> &[u8] {
-        if self.choose_a {
-            self.a.value()
-        } else {
-            self.b.value()
-        }
+        unimplemented!()
     }
 
     fn is_valid(&self) -> bool {
-        if self.choose_a {
-            self.a.is_valid()
-        } else {
-            self.b.is_valid()
-        }
+        unimplemented!()
     }
 
     fn next(&mut self) -> Result<()> {
-        if self.choose_a {
-            self.a.next()?;
-        } else {
-            self.b.next()?;
+        unimplemented!()
+    }
+}
+
+
+
+#[derive(Clone)]
+pub struct MockIterator {
+    pub data: Vec<(Bytes, Bytes)>,
+    pub index: usize,
+}
+
+impl MockIterator {
+    pub fn new(data: Vec<(Bytes, Bytes)>) -> Self {
+        Self { data, index: 0 }
+    }
+}
+
+impl StorageIterator for MockIterator {
+    fn next(&mut self) -> Result<()> {
+        if self.index < self.data.len() {
+            self.index += 1;
         }
-        self.skip_b()?;
-        self.choose_a = Self::choose_a(&self.a, &self.b);
         Ok(())
     }
+
+    fn key(&self) -> &[u8] {
+        self.data[self.index].0.as_ref()
+    }
+
+    fn value(&self) -> &[u8] {
+        self.data[self.index].1.as_ref()
+    }
+
+    fn is_valid(&self) -> bool {
+        self.index < self.data.len()
+    }
+}
+
+fn as_bytes(x: &[u8]) -> Bytes {
+    Bytes::copy_from_slice(x)
+}
+
+fn check_iter_result(iter: impl StorageIterator, expected: Vec<(Bytes, Bytes)>) {
+    let mut iter = iter;
+    for (k, v) in expected {
+        assert!(iter.is_valid());
+        assert_eq!(
+            k,
+            iter.key(),
+            "expected key: {:?}, actual key: {:?}",
+            k,
+            as_bytes(iter.key()),
+        );
+        assert_eq!(
+            v,
+            iter.value(),
+            "expected value: {:?}, actual value: {:?}",
+            v,
+            as_bytes(iter.value()),
+        );
+        iter.next().unwrap();
+    }
+    assert!(!iter.is_valid());
+}
+
+#[test]
+fn test_merge_1() {
+    let i1 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.1")),
+        (Bytes::from("b"), Bytes::from("2.1")),
+        (Bytes::from("c"), Bytes::from("3.1")),
+    ]);
+    let i2 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.2")),
+        (Bytes::from("b"), Bytes::from("2.2")),
+        (Bytes::from("c"), Bytes::from("3.2")),
+        (Bytes::from("d"), Bytes::from("4.2")),
+    ]);
+    let i3 = MockIterator::new(vec![
+        (Bytes::from("b"), Bytes::from("2.3")),
+        (Bytes::from("c"), Bytes::from("3.3")),
+        (Bytes::from("d"), Bytes::from("4.3")),
+    ]);
+
+    let iter = MergeIterator::create(vec![
+        Box::new(i1.clone()),
+        Box::new(i2.clone()),
+        Box::new(i3.clone()),
+    ]);
+
+    check_iter_result(
+        iter,
+        vec![
+            (Bytes::from("a"), Bytes::from("1.1")),
+            (Bytes::from("b"), Bytes::from("2.1")),
+            (Bytes::from("c"), Bytes::from("3.1")),
+            (Bytes::from("d"), Bytes::from("4.2")),
+        ],
+    );
+
+    let iter = MergeIterator::create(vec![Box::new(i3), Box::new(i1), Box::new(i2)]);
+
+    check_iter_result(
+        iter,
+        vec![
+            (Bytes::from("a"), Bytes::from("1.1")),
+            (Bytes::from("b"), Bytes::from("2.3")),
+            (Bytes::from("c"), Bytes::from("3.3")),
+            (Bytes::from("d"), Bytes::from("4.3")),
+        ],
+    );
+}
+
+#[test]
+fn test_merge_2() {
+    let i1 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.1")),
+        (Bytes::from("b"), Bytes::from("2.1")),
+        (Bytes::from("c"), Bytes::from("3.1")),
+    ]);
+    let i2 = MockIterator::new(vec![
+        (Bytes::from("d"), Bytes::from("1.2")),
+        (Bytes::from("e"), Bytes::from("2.2")),
+        (Bytes::from("f"), Bytes::from("3.2")),
+        (Bytes::from("g"), Bytes::from("4.2")),
+    ]);
+    let i3 = MockIterator::new(vec![
+        (Bytes::from("h"), Bytes::from("1.3")),
+        (Bytes::from("i"), Bytes::from("2.3")),
+        (Bytes::from("j"), Bytes::from("3.3")),
+        (Bytes::from("k"), Bytes::from("4.3")),
+    ]);
+    let i4 = MockIterator::new(vec![]);
+    let result = vec![
+        (Bytes::from("a"), Bytes::from("1.1")),
+        (Bytes::from("b"), Bytes::from("2.1")),
+        (Bytes::from("c"), Bytes::from("3.1")),
+        (Bytes::from("d"), Bytes::from("1.2")),
+        (Bytes::from("e"), Bytes::from("2.2")),
+        (Bytes::from("f"), Bytes::from("3.2")),
+        (Bytes::from("g"), Bytes::from("4.2")),
+        (Bytes::from("h"), Bytes::from("1.3")),
+        (Bytes::from("i"), Bytes::from("2.3")),
+        (Bytes::from("j"), Bytes::from("3.3")),
+        (Bytes::from("k"), Bytes::from("4.3")),
+    ];
+
+    let iter = MergeIterator::create(vec![
+        Box::new(i1.clone()),
+        Box::new(i2.clone()),
+        Box::new(i3.clone()),
+        Box::new(i4.clone()),
+    ]);
+    check_iter_result(iter, result.clone());
+
+    let iter = MergeIterator::create(vec![
+        Box::new(i2.clone()),
+        Box::new(i4.clone()),
+        Box::new(i3.clone()),
+        Box::new(i1.clone()),
+    ]);
+    check_iter_result(iter, result.clone());
+
+    let iter = MergeIterator::create(vec![Box::new(i4), Box::new(i3), Box::new(i2), Box::new(i1)]);
+    check_iter_result(iter, result);
+}
+
+#[test]
+fn test_merge_empty() {
+    let iter = MergeIterator::<MockIterator>::create(vec![]);
+    check_iter_result(iter, vec![]);
+}
+
+fn check_iter_result_two(iter: impl StorageIterator, expected: Vec<(Bytes, Bytes)>) {
+    let mut iter = iter;
+    for (k, v) in expected {
+        assert!(iter.is_valid());
+        assert_eq!(iter.key(), k.as_ref());
+        assert_eq!(iter.value(), v.as_ref());
+        iter.next().unwrap();
+    }
+    assert!(!iter.is_valid());
+}
+
+#[test]
+fn test_merge_1_two() {
+    let i1 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.1")),
+        (Bytes::from("b"), Bytes::from("2.1")),
+        (Bytes::from("c"), Bytes::from("3.1")),
+    ]);
+    let i2 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.2")),
+        (Bytes::from("b"), Bytes::from("2.2")),
+        (Bytes::from("c"), Bytes::from("3.2")),
+        (Bytes::from("d"), Bytes::from("4.2")),
+    ]);
+    let iter = TwoMergeIterator::create(i1, i2).unwrap();
+    check_iter_result_two(
+        iter,
+        vec![
+            (Bytes::from("a"), Bytes::from("1.1")),
+            (Bytes::from("b"), Bytes::from("2.1")),
+            (Bytes::from("c"), Bytes::from("3.1")),
+            (Bytes::from("d"), Bytes::from("4.2")),
+        ],
+    )
+}
+
+#[test]
+fn test_merge_2_two() {
+    let i2 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.1")),
+        (Bytes::from("b"), Bytes::from("2.1")),
+        (Bytes::from("c"), Bytes::from("3.1")),
+    ]);
+    let i1 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.2")),
+        (Bytes::from("b"), Bytes::from("2.2")),
+        (Bytes::from("c"), Bytes::from("3.2")),
+        (Bytes::from("d"), Bytes::from("4.2")),
+    ]);
+    let iter = TwoMergeIterator::create(i1, i2).unwrap();
+    check_iter_result_two(
+        iter,
+        vec![
+            (Bytes::from("a"), Bytes::from("1.2")),
+            (Bytes::from("b"), Bytes::from("2.2")),
+            (Bytes::from("c"), Bytes::from("3.2")),
+            (Bytes::from("d"), Bytes::from("4.2")),
+        ],
+    )
+}
+
+#[test]
+fn test_merge_3_two() {
+    let i2 = MockIterator::new(vec![
+        (Bytes::from("a"), Bytes::from("1.1")),
+        (Bytes::from("b"), Bytes::from("2.1")),
+        (Bytes::from("c"), Bytes::from("3.1")),
+    ]);
+    let i1 = MockIterator::new(vec![
+        (Bytes::from("b"), Bytes::from("2.2")),
+        (Bytes::from("c"), Bytes::from("3.2")),
+        (Bytes::from("d"), Bytes::from("4.2")),
+    ]);
+    let iter = TwoMergeIterator::create(i1, i2).unwrap();
+    check_iter_result_two(
+        iter,
+        vec![
+            (Bytes::from("a"), Bytes::from("1.1")),
+            (Bytes::from("b"), Bytes::from("2.2")),
+            (Bytes::from("c"), Bytes::from("3.2")),
+            (Bytes::from("d"), Bytes::from("4.2")),
+        ],
+    )
+}
+
+#[test]
+fn test_merge_4_two() {
+    let i2 = MockIterator::new(vec![]);
+    let i1 = MockIterator::new(vec![
+        (Bytes::from("b"), Bytes::from("2.2")),
+        (Bytes::from("c"), Bytes::from("3.2")),
+        (Bytes::from("d"), Bytes::from("4.2")),
+    ]);
+    let iter = TwoMergeIterator::create(i1, i2).unwrap();
+    check_iter_result_two(
+        iter,
+        vec![
+            (Bytes::from("b"), Bytes::from("2.2")),
+            (Bytes::from("c"), Bytes::from("3.2")),
+            (Bytes::from("d"), Bytes::from("4.2")),
+        ],
+    );
+    let i1 = MockIterator::new(vec![]);
+    let i2 = MockIterator::new(vec![
+        (Bytes::from("b"), Bytes::from("2.2")),
+        (Bytes::from("c"), Bytes::from("3.2")),
+        (Bytes::from("d"), Bytes::from("4.2")),
+    ]);
+    let iter = TwoMergeIterator::create(i1, i2).unwrap();
+    check_iter_result_two(
+        iter,
+        vec![
+            (Bytes::from("b"), Bytes::from("2.2")),
+            (Bytes::from("c"), Bytes::from("3.2")),
+            (Bytes::from("d"), Bytes::from("4.2")),
+        ],
+    );
+}
+
+#[test]
+fn test_merge_5_two() {
+    let i2 = MockIterator::new(vec![]);
+    let i1 = MockIterator::new(vec![]);
+    let iter = TwoMergeIterator::create(i1, i2).unwrap();
+    check_iter_result_two(iter, vec![])
 }
