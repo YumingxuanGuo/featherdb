0bcbde0ae19e537fee84b07bbda4e5d7a4af8137
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-15 14:04:23 -0500

    added wrapper for TwoMergeIter that skips tombstoned entreis

--- src/storage/kv/structure/lsm_tree/lsm_iterator.rs
+++ src/storage/kv/structure/lsm_tree/lsm_iterator.rs
@@ -1,105 +1,64 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
-use std::ops::Bound;
-
-use bytes::Bytes;
-
 use crate::error::Result;
 
-use super::{iterators::{StorageIterator, TwoMergeIterator, MergeIterator}, memtable::MemTableIterator, sstable::SsTableIterator};
+use super::iterators::{TwoMergeIter, MergeIter, StorageIter};
+use super::memtable::MemTableIter;
+use super::sstable::SsTableIter;
 
-type LsmIteratorInner =
-    TwoMergeIterator<MergeIterator<MemTableIterator>, MergeIterator<SsTableIterator>>;
+type LsmIterInner = TwoMergeIter<MergeIter<MemTableIter>, MergeIter<SsTableIter>>;
 
-pub struct LsmIterator {
-    inner_iter: LsmIteratorInner,
-    end_bound: Bound<Bytes>,
-    is_valid: bool,
+#[derive(Clone)]
+pub struct LsmIter {
+    inner_iter: LsmIterInner,
 }
 
-impl LsmIterator {
-    pub(crate) fn new(inner_iter: LsmIteratorInner, end_bound: Bound<Bytes>) -> Result<Self> {
-        let mut this = Self {
-            is_valid: inner_iter.is_valid(),
-            inner_iter,
-            end_bound,
-        };
-        this.move_to_non_delete()?;
-        Ok(this)
+impl LsmIter {
+    pub fn create(inner_iter: LsmIterInner) -> Self {
+        Self { inner_iter }
+    }
+}
+
+impl StorageIter for LsmIter {
+    fn front_entry(&self) -> Option<(Vec<u8>, Vec<u8>)> {
+        self.inner_iter.front_entry()
     }
 
-    fn next_inner(&mut self) -> Result<()> {
-        self.inner_iter.next()?;
-        if !self.inner_iter.is_valid() {
-            self.is_valid = false;
-            return Ok(());
-        }
-        match self.end_bound.as_ref() {
-            Bound::Unbounded => {}
-            Bound::Included(key) => self.is_valid = self.inner_iter.key() <= key.as_ref(),
-            Bound::Excluded(key) => self.is_valid = self.inner_iter.key() < key.as_ref(),
-        }
-        Ok(())
+    fn back_entry(&self) -> Option<(Vec<u8>, Vec<u8>)> {
+        self.inner_iter.back_entry()
     }
+
+    fn is_valid(&self) -> bool {
+        self.inner_iter.is_valid()
+    }
+
+    fn try_next(&mut self) -> Result<Option<(Vec<u8>, Vec<u8>)>> {
+        while let Some((key, value)) = self.inner_iter.try_next()? {
+            if !value.is_empty() {
+                return Ok(Some((key, value)));
+            }
+        }
+        Ok(None)
+    }
+
+    fn try_next_back(&mut self) -> Result<Option<(Vec<u8>, Vec<u8>)>> {
+        while let Some((key, value)) = self.inner_iter.try_next_back()? {
+            if !value.is_empty() {
+                return Ok(Some((key, value)));
+            }
+        }
+        Ok(None)
+    }
+}
+
+impl Iterator for LsmIter {
+    type Item = Result<(Vec<u8>, Vec<u8>)>;
     
-    fn move_to_non_delete(&mut self) -> Result<()> {
-        while self.is_valid() && self.inner_iter.value().is_empty() {
-            self.next_inner()?;
-        }
-        Ok(())
+    fn next(&mut self) -> Option<Self::Item> {
+        self.try_next().transpose()
     }
 }
 
-impl StorageIterator for LsmIterator {
-    fn is_valid(&self) -> bool {
-        self.is_valid
-    }
-
-    fn key(&self) -> &[u8] {
-        self.inner_iter.key()
-    }
-
-    fn value(&self) -> &[u8] {
-        self.inner_iter.value()
-    }
-
-    fn next(&mut self) -> Result<()> {
-        self.next_inner()?;
-        self.move_to_non_delete()
-    }
-}
-
-/// A wrapper around existing iterator, will prevent users from calling `next` when the iterator is
-/// invalid.
-pub struct FusedIterator<I: StorageIterator> {
-    iter: I,
-}
-
-impl<I: StorageIterator> FusedIterator<I> {
-    pub fn new(iter: I) -> Self {
-        Self { iter }
-    }
-}
-
-impl<I: StorageIterator> StorageIterator for FusedIterator<I> {
-    fn is_valid(&self) -> bool {
-        self.iter.is_valid()
-    }
-
-    fn key(&self) -> &[u8] {
-        self.iter.key()
-    }
-
-    fn value(&self) -> &[u8] {
-        self.iter.value()
-    }
-
-    fn next(&mut self) -> Result<()> {
-        // only move when the iterator is valid
-        if self.iter.is_valid() {
-            self.iter.next()?;
-        }
-        Ok(())
+impl DoubleEndedIterator for LsmIter {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.try_next_back().transpose()
     }
 }
