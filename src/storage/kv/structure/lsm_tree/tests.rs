fc3bcbb150a50aae2f097c88882c84888d4eaf5e
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-16 22:39:08 -0500

    added test cases for LsmStorage concurrent operations and inaccurate range scan

--- src/storage/kv/structure/lsm_tree/tests.rs
+++ src/storage/kv/structure/lsm_tree/tests.rs
@@ -17,10 +17,8 @@ fn as_bytes(x: &[u8]) -> Bytes {
 
 #[cfg(test)]
 fn check_iter_result(iter: KvScan, expected: Vec<(Bytes, Bytes)>) {
-
     let mut iter = iter;
     for (k, v) in expected {
-        // assert!(iter.is_valid());
         let (key, value) = iter.next().unwrap().unwrap();
         assert_eq!(
             k,
@@ -37,7 +35,7 @@ fn check_iter_result(iter: KvScan, expected: Vec<(Bytes, Bytes)>) {
             as_bytes(&value),
         );
     }
-    // assert!(!iter.is_valid());
+    assert!(iter.next().is_none());
 }
 
 #[test]
@@ -162,7 +160,7 @@ fn test_storage_scan_memtable_1_after_sync() {
         storage
             .scan(Range::from(b"1".to_vec()..b"3".to_vec()))
             .unwrap(),
-        vec![],
+        vec![(Bytes::from("1"), Bytes::from("233"))],
     );
 }
 
@@ -196,4 +194,137 @@ fn test_storage_scan_memtable_2_after_sync() {
             .unwrap(),
         vec![(Bytes::from("2"), Bytes::from("2333"))],
     );
+}
+
+#[test]
+fn test_storage_empty() {
+    use super::lsm_storage::LsmStorage;
+    let dir = tempdir().unwrap();
+    let storage = LsmStorage::open(&dir).unwrap();
+
+    assert!(storage.get(b"1").unwrap().is_none());
+    assert!(storage.get(b"2").unwrap().is_none());
+    storage.delete(b"1").unwrap();
+    assert!(storage.get(b"1").unwrap().is_none());
+    assert!(storage.get(b"2").unwrap().is_none());
+}
+
+#[test]
+fn test_storage_duplicate_key() {
+    use super::lsm_storage::LsmStorage;
+    let dir = tempdir().unwrap();
+    let storage = LsmStorage::open(&dir).unwrap();
+
+    storage.set(b"1", b"233".to_vec()).unwrap();
+    storage.set(b"2", b"2333".to_vec()).unwrap();
+    storage.set(b"3", b"23333".to_vec()).unwrap();
+    assert_eq!(&storage.get(b"1").unwrap().unwrap()[..], b"233");
+    assert_eq!(&storage.get(b"2").unwrap().unwrap()[..], b"2333");
+    assert_eq!(&storage.get(b"3").unwrap().unwrap()[..], b"23333");
+
+    storage.flush().unwrap();
+    storage.set(b"1", b"new_value1".to_vec()).unwrap();
+    storage.set(b"2", b"new_value2".to_vec()).unwrap();
+    assert_eq!(&storage.get(b"1").unwrap().unwrap()[..], b"new_value1");
+    assert_eq!(&storage.get(b"2").unwrap().unwrap()[..], b"new_value2");
+    assert_eq!(&storage.get(b"3").unwrap().unwrap()[..], b"23333");
+
+    storage.delete(b"2").unwrap();
+    assert_eq!(&storage.get(b"1").unwrap().unwrap()[..], b"new_value1");
+    assert!(storage.get(b"2").unwrap().is_none());
+    assert_eq!(&storage.get(b"3").unwrap().unwrap()[..], b"23333");
+
+    storage.set(b"2", b"new_value_after_deletion".to_vec()).unwrap();
+    assert_eq!(&storage.get(b"1").unwrap().unwrap()[..], b"new_value1");
+    assert_eq!(&storage.get(b"2").unwrap().unwrap()[..], b"new_value_after_deletion");
+    assert_eq!(&storage.get(b"3").unwrap().unwrap()[..], b"23333");
+}
+
+#[test]
+fn test_storage_scan_inaccurate_range() {
+    use super::lsm_storage::LsmStorage;
+    let dir = tempdir().unwrap();
+    let storage = LsmStorage::open(&dir).unwrap();
+    storage.set(b"1", b"1".to_vec()).unwrap();
+    storage.set(b"3", b"3".to_vec()).unwrap();
+    storage.set(b"5", b"5".to_vec()).unwrap();
+    storage.flush().unwrap();
+    storage.set(b"7", b"7".to_vec()).unwrap();
+    storage.set(b"9", b"9".to_vec()).unwrap();
+    storage.flush().unwrap();
+    storage.delete(b"1").unwrap();
+    check_iter_result(
+        storage.scan(Range::from(..)).unwrap(),
+        vec![
+            (Bytes::from("3"), Bytes::from("3")),
+            (Bytes::from("5"), Bytes::from("5")),
+            (Bytes::from("7"), Bytes::from("7")),
+            (Bytes::from("9"), Bytes::from("9")),
+        ],
+    );
+    check_iter_result(
+        storage
+            .scan(Range::from(b"1".to_vec()..=b"2".to_vec()))
+            .unwrap(),
+        vec![],
+    );
+    check_iter_result(
+        storage
+            .scan(Range::from(b"2".to_vec()..b"6".to_vec()))
+            .unwrap(),
+        vec![
+            (Bytes::from("3"), Bytes::from("3")),
+            (Bytes::from("5"), Bytes::from("5")),
+        ],
+    );
+    storage.delete(b"7").unwrap();
+    check_iter_result(
+        storage
+            .scan(Range::from(b"2".to_vec()..=b"9".to_vec()))
+            .unwrap(),
+        vec![
+            (Bytes::from("3"), Bytes::from("3")),
+            (Bytes::from("5"), Bytes::from("5")),
+            (Bytes::from("9"), Bytes::from("9")),
+        ],
+    );
+}
+
+#[cfg(test)]
+fn key_of(idx: usize) -> Vec<u8> {
+    format!("key_{:04}", idx * 3).into_bytes()
+}
+
+#[cfg(test)]
+fn value_of(idx: usize) -> Vec<u8> {
+    format!("value_{:010}", idx).into_bytes()
+}
+
+#[test]
+fn test_storage_concurrent_operation() {
+    use std::thread;
+    use std::sync::Arc;
+    use super::lsm_storage::LsmStorage;
+    let dir = tempdir().unwrap();
+    let storage = Arc::new(LsmStorage::open(&dir).unwrap());
+
+    let mut handles = vec![];
+    for i in 0..10 {
+        let storage = Arc::clone(&storage);
+        let handle = thread::spawn(move || {
+            for j in 0..100 {
+                storage.set(&key_of(j * 10 + i), value_of(j * 10 + i)).unwrap();
+                assert_eq!(&storage.get(&key_of(j * 10 + i)).unwrap().unwrap(), &value_of(j * 10 + i));
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    for i in 0..1000 {
+        assert_eq!(&storage.get(&key_of(i)).unwrap().unwrap(), &value_of(i));
+    }
 }
