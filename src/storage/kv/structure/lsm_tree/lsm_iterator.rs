876540bdf9c7a93276d0691526f920c5284cdb79
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-16 21:31:40 -0500

    added test cases for duplicate keys

--- src/storage/kv/structure/lsm_tree/lsm_iterator.rs
+++ src/storage/kv/structure/lsm_tree/lsm_iterator.rs
@@ -196,7 +196,7 @@ fn test_lsm_iterator_empty() {
 }
 
 #[cfg(test)]
-fn generate_memtable_mergeiter(scales: &Vec<usize>, expected: Vec<(Vec<u8>, Vec<u8>)>) -> MergeIter<MemTableIter> {
+fn generate_memtable_mergeiter(scales: Vec<usize>, expected: Vec<(Vec<u8>, Vec<u8>)>) -> MergeIter<MemTableIter> {
     let mut index = 0;
     let mut memtable_iters = vec![];
     for table_size in scales {
@@ -220,7 +220,7 @@ fn test_lsm_iterator_multiple_memtable_of_scale(scales: Vec<usize>) {
     }
     expected.shuffle(&mut thread_rng());
 
-    let memtable_merge_iter = generate_memtable_mergeiter(&scales, expected.clone());
+    let memtable_merge_iter = generate_memtable_mergeiter(scales, expected.clone());
     let sstable_merge_iter = MergeIter::create(Vec::<Box::<SsTableIter>>::new()).unwrap();
     let lsm_iterator = LsmIter::create(TwoMergeIter::create(
         memtable_merge_iter, sstable_merge_iter
@@ -245,8 +245,10 @@ fn test_lsm_iterator_memtable_only() {
 #[cfg(test)]
 fn generate_sst(entries: &[(Vec<u8>, Vec<u8>)]) -> SsTable {
     let mut builder = SsTableBuilder::new(128);
+    let mut entries = entries.to_vec();
+    entries.sort();
     for (key, value) in entries {
-        builder.add(key, value);
+        builder.add(&key, &value);
     }
     let dir = tempdir().unwrap();
     let path = dir.path().join("1.sst");
@@ -254,11 +256,10 @@ fn generate_sst(entries: &[(Vec<u8>, Vec<u8>)]) -> SsTable {
 }
 
 #[cfg(test)]
-fn generate_sstable_mergeiter(scales: Vec<usize>, mut expected: Vec<(Vec<u8>, Vec<u8>)>) -> MergeIter<SsTableIter> {
+fn generate_sstable_mergeiter(scales: Vec<usize>, expected: Vec<(Vec<u8>, Vec<u8>)>) -> MergeIter<SsTableIter> {
     use std::sync::Arc;
     let mut index = 0;
     let mut sstable_iters = vec![];
-    expected.sort();
     for table_size in scales {
         let sstable = generate_sst(&expected[index..(index + table_size)]);
         sstable_iters.push(Box::new(SsTableIter::new(Arc::new(sstable)).unwrap()));
@@ -299,10 +300,90 @@ fn test_lsm_iterator_sstable_only() {
     test_lsm_iterator_multiple_sstable_of_scale(vec![1, 500, 3, 1, 100]);
 }
 
-
 // Multi-level LSM tree
+#[cfg(test)]
+fn test_lsm_iterator_hybrid_tables_no_duplicate(memtable_scales: Vec<usize>, sstable_scales: Vec<usize>) {
+    use rand::thread_rng;
+    use rand::seq::SliceRandom;
+    let mut expected = vec![];
+    let memtable_entry_num = memtable_scales.iter().sum::<usize>();
+    let sstable_entry_num = sstable_scales.iter().sum::<usize>();
+    let total_entry_num = memtable_entry_num + sstable_entry_num;
+    for i in 0..total_entry_num {
+        expected.push(((key_of(i)), value_of(i)));
+    }
+    expected.shuffle(&mut thread_rng());
+
+    let memtable_expected = expected[0..memtable_entry_num].to_vec();
+    let sstable_expected = expected[memtable_entry_num..].to_vec();
+
+    let memtable_merge_iter = generate_memtable_mergeiter(memtable_scales, memtable_expected);
+    let sstable_merge_iter = generate_sstable_mergeiter(sstable_scales, sstable_expected);
+    let lsm_iterator = LsmIter::create(TwoMergeIter::create(
+        memtable_merge_iter, sstable_merge_iter
+    ).unwrap());
+
+    check_result(lsm_iterator, {expected.sort(); expected}); 
+}
+
+#[test]
+fn test_lsm_iterator_hybrid_tables() {
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![1], vec![1]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![100], vec![1]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![1], vec![100]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![100], vec![100]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![1, 1], vec![1, 1, 1, 1, 1]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![1, 2], vec![1, 1, 1, 1, 1]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![1, 1, 1, 1, 1], vec![1, 1]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![3, 5, 1, 2, 4], vec![1, 2]);
+    test_lsm_iterator_hybrid_tables_no_duplicate(vec![30, 50, 1, 2, 40], vec![1, 500, 3, 1, 100]);
+}
 
 // Duplicate keys
+#[cfg(test)]
+fn test_lsm_iterator_hybrid_tables_with_duplicate(memtable_scales: Vec<usize>, sstable_scales: Vec<usize>) {
+    use std::collections::HashSet;
+    use rand::thread_rng;
+    use rand::seq::{SliceRandom, IteratorRandom};
+    let mut expected = vec![];
+    let memtable_entry_num = memtable_scales.iter().sum::<usize>();
+    let sstable_entry_num = sstable_scales.iter().sum::<usize>();
+    let total_entry_num = memtable_entry_num + sstable_entry_num;
+    for i in 0..total_entry_num {
+        expected.push(((key_of(i)), value_of(i)));
+    }
+    expected.shuffle(&mut thread_rng());
+
+    let mut memtable_expected = expected.clone().into_iter().choose_multiple(&mut thread_rng(), memtable_entry_num);
+    let mut sstable_expected = expected.clone().into_iter().choose_multiple(&mut thread_rng(), sstable_entry_num);
+
+    let memtable_merge_iter = generate_memtable_mergeiter(memtable_scales, memtable_expected.clone());
+    let sstable_merge_iter = generate_sstable_mergeiter(sstable_scales, sstable_expected.clone());
+    let lsm_iterator = LsmIter::create(TwoMergeIter::create(
+        memtable_merge_iter, sstable_merge_iter
+    ).unwrap());
+
+    memtable_expected.append(&mut sstable_expected);
+    let mut unique_expected = memtable_expected.into_iter().collect::<HashSet<_>>().into_iter().collect::<Vec<_>>();
+    unique_expected.sort();
+
+    check_result(lsm_iterator, unique_expected); 
+}
+
+#[test]
+fn test_lsm_iterator_with_duplicate_keys() {
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![1], vec![1]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![100], vec![1]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![1], vec![100]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![100], vec![100]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![1, 1], vec![1, 1, 1, 1, 1]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![1, 2], vec![1, 1, 1, 1, 1]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![1, 1, 1, 1, 1], vec![1, 1]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![3, 5, 1, 2, 4], vec![1, 2]);
+    test_lsm_iterator_hybrid_tables_with_duplicate(vec![300, 500, 1, 2, 40], vec![1, 500, 3, 1, 100]);
+}
+
+
 // Key range iteration
 // Iteration order
 // Concurrent modifications
