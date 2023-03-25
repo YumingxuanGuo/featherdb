777aa87365fb65bacebb825b61ad36fbc5531493
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-25 14:28:58 -0500

    added validate() for Table and Column

--- src/sql/schema.rs
+++ src/sql/schema.rs
@@ -5,7 +5,8 @@
 use serde_derive::{Deserialize, Serialize};
 
 use crate::error::{Error, Result};
-use super::types::DataType;
+use super::types::{DataType, Value};
+use super::engine::SqlTxn;
 
 /// The catalog stores schema information
 pub trait Catalog {
@@ -52,7 +53,43 @@ pub struct Table {
 }
 
 impl Table {
-    
+    /// Creates a new table schema.
+    pub fn new(name: String, columns: Vec<Column>) -> Result<Self> {
+        Ok(Self { name, columns })
+    }
+
+    /// Returns the primary key value of a row.
+    pub fn get_row_key(&self, row: &[Value]) -> Result<Value> {
+        todo!()
+    }
+
+    /// Validates a table schema.
+    pub fn validate(&self, txn: &mut dyn SqlTxn) -> Result<()> {
+        if self.columns.is_empty() {
+            return Err(Error::Value(format!("Table {} has no columns", self.name)));
+        }
+        match self.columns.iter().filter(|column| column.is_primary_key).count() {
+            1 => { },
+            0 => return Err(Error::Value(format!("Table {} has no primary key", self.name))),
+            _ => return Err(Error::Value(format!("Table {} has multiple primary keys", self.name))),
+        };
+        for column in &self.columns {
+            column.validate(self, txn)?;
+        }
+        Ok(())
+    }
+
+    /// Validates a row.
+    pub fn validate_row(&self, row: &[Value], txn: &mut dyn SqlTxn) -> Result<()> {
+        if row.len() != self.columns.len() {
+            return Err(Error::Value(format!("Row has {} columns, expected {}", row.len(), self.columns.len())));
+        }
+        let primary_key = self.get_row_key(row)?;
+        for (column, value) in self.columns.iter().zip(row.iter()) {
+            column.validate_value(value, &primary_key, self, txn)?;
+        }
+        Ok(())
+    }
 }
 
 /// A table scan iterator
@@ -68,17 +105,31 @@ pub struct Column {
     /// Whether the column is a primary key
     pub is_primary_key: bool,
     /// Whether the column allows null values
-    pub nullable: bool,
-    // /// The default value of the column
-    // pub default: Option<Value>,
+    pub is_nullable: bool,
+    /// The default value of the column
+    pub default: Option<Value>,
     /// Whether the column should only take unique values
-    pub unique: bool,
+    pub is_unique: bool,
     /// The table which is referenced by this foreign key
     pub references: Option<String>,
     /// Whether the column should be indexed
-    pub index: bool,
+    pub is_indexed: bool,
 }
 
 impl Column {
-    
+    /// Validates a column schema.
+    pub fn validate(&self, table: &Table, txn: &mut dyn SqlTxn) -> Result<()> {
+        todo!()
+    }
+
+    /// Validates a column value.
+    pub fn validate_value(
+        &self, 
+        value: &Value, 
+        primary_key: &Value,
+        table: &Table, 
+        txn: &mut dyn SqlTxn
+    ) -> Result<()> {
+        todo!()
+    }
 }
