0af32ebdfce00beea488149b72c2ccee4188e808
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-27 17:36:01 -0500

    added skeletons for Expression::evaluate()

--- src/sql/types/expression.rs
+++ src/sql/types/expression.rs
@@ -2,8 +2,17 @@ use serde_derive::{Deserialize, Serialize};
 
 use crate::error::{Error, Result};
 
+use super::{Row, Value};
+
 /// An expression, made up of constants and operations
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum Expression {
 
+}
+
+impl Expression {
+    /// Evaluates an expression to a value, given an environment.
+    pub fn evaluate(&self, env: Option<&Row>) -> Result<Value> {
+        todo!()
+    }
 }
