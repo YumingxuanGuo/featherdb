bb7275cfdc2e6dfa3eda606beacd16214ad6398c
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-04-08 13:30:22 -0500

    updated ProtoBuf build script

--- build.rs
+++ build.rs
@@ -2,12 +2,6 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile(&["src/proto/hello_world.proto"], &["proto"])
-        .unwrap();
-
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
         .compile(&["src/proto/database.proto"], &["proto"])
         .unwrap();
 }
