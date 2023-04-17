16ce7fc6465249991a303c31d399376686351350
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-04-17 09:24:03 -0500

    implemented append_entries RPC handler and outer interfaces

--- src/raft/node.rs
+++ src/raft/node.rs
@@ -8,8 +8,9 @@ use futures::{stream::FuturesUnordered, Future};
 use tokio_stream::StreamExt;
 use tonic::{Response, Status, Request};
 
-use crate::{error::Result, proto::raft::raft_service_server::RaftService};
-use crate::proto::raft::{RequestVoteReply, RequestVoteArgs};
+use crate::error::{Result, Error};
+use crate::proto::raft::raft_service_server::RaftService;
+use crate::proto::raft::{RequestVoteReply, RequestVoteArgs, AppendEntriesArgs, AppendEntriesReply};
 use super::{Raft, Role, HEARTBEAT_INTERVAL};
 
 #[derive(Clone)]
@@ -22,6 +23,39 @@ impl Node {
     /// Create a new raft service.
     pub fn new(raft: Raft) -> Node {
         Node { raft: Arc::new(Mutex::new(raft)) }
+    }
+
+    /// The service using Raft (e.g. a k/v server) wants to start
+    /// agreement on the next command to be appended to Raft's log. If this
+    /// server isn't the leader, returns [`Error::NotLeader`]. Otherwise start
+    /// the agreement and return immediately. There is no guarantee that this
+    /// command will ever be committed to the Raft log, since the leader
+    /// may fail or lose an election. Even if the Raft instance has been killed,
+    /// this function should return gracefully.
+    ///
+    /// The first value of the tuple is the index that the command will appear
+    /// at if it's ever committed; the second is the current term.
+    ///
+    /// This method must return without blocking on the raft.
+    pub fn start<M>(&self, command: &M) -> Result<(u64, u64)>
+    // where
+    //     M: labcodec::Message,
+    {
+        let mut raft = self.raft.lock()?;
+        if !raft.is_leader() {
+            return Err(Error::NotLeader);
+        }
+        raft.start(command)
+    }
+
+    /// The current term of this peer.
+    pub fn term(&self) -> Result<u64> {
+        Ok(self.raft.lock()?.current_term)
+    }
+
+    /// Whether this peer believes it is the leader.
+    pub fn is_leader(&self) -> Result<bool> {
+        Ok(self.raft.lock()?.is_leader())
     }
 
     /// Tick the underlying Raft node to the next state.
@@ -59,7 +93,7 @@ impl Node {
             *heartbeat_ticks += 1;
                 if *heartbeat_ticks >= HEARTBEAT_INTERVAL {
                     *heartbeat_ticks = 0;
-                    // TODO: Send heartbeats.
+                    raft.send_heartbeats();
                 }
             }
         }
@@ -139,8 +173,30 @@ impl RaftService for Node {
     /// AppendEntries RPC handler.
     async fn append_entries(
         &self,
-        request: tonic::Request<crate::proto::raft::AppendEntriesArgs>,
-    ) -> core::result::Result<tonic::Response<crate::proto::raft::AppendEntriesReply>, tonic::Status> {
-        todo!()
+        request: Request<AppendEntriesArgs>,
+    ) -> core::result::Result<Response<AppendEntriesReply>, Status> {
+        let mut raft = self.raft.lock().unwrap();
+        let args = request.into_inner();
+
+        if args.term < raft.current_term {
+            let reply = AppendEntriesReply {
+                term: raft.current_term,
+                success: false
+            };
+            return Ok(Response::new(reply));
+        }
+
+        raft.become_follower(args.term, None);
+
+        if let Role::Follower { ref mut leader, ref mut leader_seen_ticks, .. } = raft.role {
+            *leader_seen_ticks = 0;
+            *leader = Some(args.leader_id);
+        }
+
+        let reply = AppendEntriesReply {
+            term: raft.current_term,
+            success: true
+        };
+        Ok(Response::new(reply))
     }
 }
