5b174ed675382347d002ed962ff97641d1ba9606
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-05-08 21:12:48 -0500

    implemented Driver's execution routine with de-duplication

--- src/raft/state.rs
+++ src/raft/state.rs
@@ -1,13 +1,14 @@
+use std::{collections::HashMap, sync::{Mutex, Arc}};
+
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
-use crate::error::Result;
-
-use super::Command;
+use crate::error::{Result, Error};
+use super::{Command, Node, KvSession, Task};
 
 /// A Raft-managed state machine.
-pub trait State: Send {
+pub trait State: Send + Sync {
     /// Returns the last applied index from the state machine, used when initializing the driver.
     fn applied_index(&self) -> u64;
 
@@ -18,46 +19,48 @@ pub trait State: Send {
 
 /// A Raft state machine apply message.
 #[derive(Debug)]
-pub enum ApplyMsg {
-    /// A command to apply.
-    Command {
-        log_index: u64,
-        command: Command,
-    },
+pub struct ApplyMsg {
+    pub log_index: u64,
+    pub command: Command,
 }
 
-/// A Raft state machine response message.
-#[derive(Debug)]
-pub enum ResponseMsg {
-    /// A response to a command.
-    Command {
-        log_index: u64,
-        session_id: u64,
-        result: Result<Vec<u8>>
-    },
+/// The meta-info for a client session.
+pub struct SesstionMeta {
+    pub session_id: u64,
+    pub last_applied_sequence_number: u64,
+    pub stored_result: Option<Result<Vec<u8>>>,
+    pub task_tx: mpsc::UnboundedSender<Task>,
+    pub result_tx: mpsc::UnboundedSender<Result<Vec<u8>>>,
 }
 
 /// Drives a state machine, taking operations from `apply_rx` and sending results via `dispatcher_tx`.
 pub struct Driver {
+    /// The underlying Raft node.
+    node: Node,
     /// The state machine.
     state: Box<dyn State>,
     /// The channel to receive state machine operations from.
     apply_rx: UnboundedReceiverStream<ApplyMsg>,
-    /// The channel to send state machine results to.
-    dispatcher_tx: mpsc::UnboundedSender<ResponseMsg>,
+    /// The channel to send registration results to.
+    registration_status: Arc<Mutex<HashMap<u64, mpsc::UnboundedSender<()>>>>,
+    /// The ongoing sessions.
+    sessions: HashMap<u64, SesstionMeta>,
 }
 
 impl Driver {
     /// Creates a new state machine driver.
     pub fn new(
+        node: Node,
         state: Box<dyn State>,
         apply_rx: mpsc::UnboundedReceiver<ApplyMsg>,
-        dispatcher_tx: mpsc::UnboundedSender<ResponseMsg>,
+        registration_status: Arc<Mutex<HashMap<u64, mpsc::UnboundedSender<()>>>>,
     ) -> Self {
         Self {
+            node,
             state,
             apply_rx: UnboundedReceiverStream::new(apply_rx),
-            dispatcher_tx,
+            registration_status,
+            sessions: HashMap::new(),
         }
     }
 
@@ -71,15 +74,73 @@ impl Driver {
         Ok(())
     }
 
-    /// Executes a state machine apply message and sends the result to the dispatcher.
+    /// Executes a state machine apply message.
     fn execute(&mut self, apply_msg: ApplyMsg) -> Result<()> {
-        match apply_msg {
-            ApplyMsg::Command { log_index, command } => {
-                let Command { session_id, operation } = command;
-                let result = self.state.execute(log_index, operation);
-                self.dispatcher_tx.send(ResponseMsg::Command { log_index, session_id, result })?;
+        let ApplyMsg { log_index, command } = apply_msg;
+        match command {
+            Command::Operation { session_id, operation, sequence_number } => {
+                let session_meta = self.sessions.get_mut(&session_id)
+                    .ok_or_else(|| Error::Internal(format!("Session {} not found", session_id)))?;
+
+                let result = 
+                    // If the operation has already been applied, returns the stored result.
+                    if session_meta.last_applied_sequence_number == sequence_number {
+                        session_meta.stored_result
+                            .clone()
+                            .ok_or_else(|| Error::Internal(format!(
+                                "No stored result for session {}",
+                                session_id,
+                            )))?
+                    }
+                    // If the operation has not been applied, applies it and stores the result.
+                    else if session_meta.last_applied_sequence_number < sequence_number {
+                        let result = self.state.execute(log_index, operation);
+                        session_meta.stored_result = Some(result.clone());
+                        session_meta.last_applied_sequence_number = sequence_number;
+                        result
+                    }
+                    // If the sequence number is smaller, returns an error.
+                    else {
+                        return Err(Error::Internal(format!(
+                            "Sequence number {} is smaller than last applied sequence number {}",
+                            sequence_number,
+                            session_meta.last_applied_sequence_number,
+                        )));
+                    };
+
+                // Sends the result to the corresponding session.
+                session_meta.result_tx.send(result)?;
+            },
+
+            Command::Registration { session_id } => {
+                // Records the meta-data of the session. TODO: Possible de-duplication.
+                let (task_tx, task_rx) = mpsc::unbounded_channel();
+                let (result_tx, result_rx) = mpsc::unbounded_channel();
+                self.sessions.insert(session_id, SesstionMeta {
+                    session_id,
+                    last_applied_sequence_number: 0,
+                    stored_result: None,
+                    task_tx,
+                    result_tx,
+                });
+
+                // Spawns a new session.
+                let node = self.node.clone();
+                tokio::spawn(async move {
+                    KvSession::new(node, session_id, task_rx, result_rx)
+                        .serve()
+                        .await
+                        .unwrap();
+                });
+
+                // Notifies the server that the session has been registered.
+                let mut registration_status = self.registration_status.lock()?;
+                let registration_tx = registration_status.remove(&session_id)
+                    .ok_or_else(|| Error::Internal(format!("Registration {} not found", session_id)))?;
+                registration_tx.send(())?;
             },
         }
+
         Ok(())
     }
 }
