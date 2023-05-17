fac4b6dc21f884deb2610e107d16af861c5679fb
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-05-17 17:17:40 -0500

    added skeletons for Command::Mutation's execution

--- src/raft/state.rs
+++ src/raft/state.rs
@@ -1,23 +1,24 @@
-use std::{collections::HashMap, sync::{Mutex, Arc}};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
-use tokio::sync::{mpsc, oneshot};
+use tokio::sync::{mpsc};
 use tokio_stream::StreamExt;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
 use crate::error::{Result, Error};
-use super::{Command, Node, KvSession, Task};
+use super::{Command, Node, Session, Task};
 
 /// A Raft-managed state machine.
 pub trait State: Send + Sync {
     /// Returns the last applied index from the state machine, used when initializing the driver.
     fn applied_index(&self) -> u64;
 
-    /// Executes the given operation. If the state machine returns Error::Internal, the Raft node
+    /// Mutates the state machine. If the state machine returns Error::Internal, the Raft node
     /// halts. For any other error, the state is applied and the error propagated to the caller.
-    fn execute(&mut self, index: u64, operation: Vec<u8>) -> Result<Vec<u8>>;
+    fn mutate(&mut self, index: u64, mutation: Vec<u8>) -> Result<Vec<u8>>;
 
     /// Queries the state machine. All errors are propagated to the caller.
-    fn query(&self, command: Vec<u8>) -> Result<Vec<u8>>;
+    fn query(&self, query: Vec<u8>) -> Result<Vec<u8>>;
 }
 
 /// A Raft state machine apply message.
@@ -49,7 +50,7 @@ pub struct Driver {
     /// The channel to receive state machine operations from.
     apply_rx: UnboundedReceiverStream<ApplyMsg>,
     /// The channel to send registration results to.
-    registration_status: Arc<Mutex<HashMap<u64, oneshot::Sender<mpsc::UnboundedSender<Task>>>>>,
+    registration_status: Arc<Mutex<HashMap<u64, mpsc::UnboundedSender<mpsc::UnboundedSender<Task>>>>>,
     /// The ongoing sessions.
     sessions: HashMap<u64, SesstionMeta>,
 }
@@ -60,7 +61,7 @@ impl Driver {
         node: Node,
         state: Box<dyn State>,
         apply_rx: mpsc::UnboundedReceiver<ApplyMsg>,
-        registration_status: Arc<Mutex<HashMap<u64, oneshot::Sender<mpsc::UnboundedSender<Task>>>>>,
+        registration_status: Arc<Mutex<HashMap<u64, mpsc::UnboundedSender<mpsc::UnboundedSender<Task>>>>>,
     ) -> Self {
         Self {
             node,
@@ -85,7 +86,7 @@ impl Driver {
     fn execute(&mut self, apply_msg: ApplyMsg) -> Result<()> {
         let ApplyMsg { log_index, command } = apply_msg;
         match command {
-            Command::Operation { session_id, operation, sequence_number } => {
+            Command::Mutation { session_id, sequence_number, mutation } => {
                 let session_meta = self.sessions.get_mut(&session_id)
                     .ok_or_else(|| Error::Internal(format!("Session {} not found", session_id)))?;
 
@@ -101,7 +102,48 @@ impl Driver {
                     }
                     // If the operation has not been applied, applies it and stores the result.
                     else if session_meta.last_applied_sequence_number < sequence_number {
-                        let result = self.state.execute(log_index, operation);
+                        let result = self.state.mutate(log_index, mutation);
+                        session_meta.stored_result = Some(result.clone());
+                        session_meta.last_applied_sequence_number = sequence_number;
+                        result
+                    }
+                    // If the sequence number is smaller, returns an error.
+                    else {
+                        return Err(Error::Internal(format!(
+                            "Sequence number {} is smaller than last applied sequence number {}",
+                            sequence_number,
+                            session_meta.last_applied_sequence_number,
+                        )));
+                    };
+
+                // If the server is the leader, sends the result to the corresponding session.
+                if self.node.is_leader()? {
+                    let apply_result = ApplyResult {
+                        sequence_number,
+                        result,
+                    };
+                    session_meta.result_tx.send(apply_result)?;
+                }
+            },
+
+            // TODO: Currently identical to Mutation; could be refactored for better performance.
+            Command::Query { session_id, sequence_number, query } => {
+                let session_meta = self.sessions.get_mut(&session_id)
+                    .ok_or_else(|| Error::Internal(format!("Session {} not found", session_id)))?;
+
+                let result = 
+                    // If the operation has already been applied, returns the stored result.
+                    if session_meta.last_applied_sequence_number == sequence_number {
+                        session_meta.stored_result
+                            .clone()
+                            .ok_or_else(|| Error::Internal(format!(
+                                "No stored result for session {}",
+                                session_id,
+                            )))?
+                    }
+                    // If the operation has not been applied, applies it and stores the result.
+                    else if session_meta.last_applied_sequence_number < sequence_number {
+                        let result = self.state.query(query);
                         session_meta.stored_result = Some(result.clone());
                         session_meta.last_applied_sequence_number = sequence_number;
                         result
@@ -143,15 +185,10 @@ impl Driver {
 
                 // Spawns a new session.
                 let node = self.node.clone();
-                tokio::spawn(async move {
-                    KvSession::new(node, session_id, task_rx, result_rx)
-                        .serve()
-                        .await
-                        .unwrap();
-                });
+                tokio::spawn(Session::new(node, session_id, task_rx, result_rx).serve());
 
                 // Notifies the server that the session has been registered.
-                let mut registration_status = self.registration_status.lock()?;
+                let mut registration_status = self.registration_status.lock().unwrap();
                 let registration_tx = registration_status.remove(&session_id)
                     .ok_or_else(|| Error::Internal(format!("Registration {} not found", session_id)))?;
                 registration_tx.send(task_tx).unwrap();
